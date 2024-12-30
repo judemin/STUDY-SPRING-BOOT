081bbee742823a9adae98e94c87155f48421a9ab
unknown <judemin2087@naver.com>
2024-12-30 15:07:01 +0900

    [INIT]: Spring Fundamental 프로젝트 Initialization

---
+++ 2412_KYH_Spring_Fundamentals_Basic/Spring_Core_Principle/src/test/java/org/example/spring_core_principle/SpringCorePrincipleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.spring_core_principle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCorePrincipleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
